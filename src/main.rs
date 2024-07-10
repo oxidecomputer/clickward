3418aabe1d1b6143408630857c71cc792d5c1d57
Andrew J. Stone <andrew@oxidecomputer.com>
2024-07-10 20:52:39 +0000

    dynamic keeper reconfig

--- src/main.rs
+++ src/main.rs
@@ -1,354 +1,14 @@
-use camino::Utf8PathBuf;
+use anyhow::{Context, Result};
+use camino::{Utf8Path, Utf8PathBuf};
 use clap::{Parser, Subcommand};
-use std::fmt::Display;
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeSet;
 use std::fs::File;
 use std::io::Write;
 use std::process::{Command, Stdio};
 
-pub enum LogLevel {
-    Trace,
-    Debug,
-}
-
-impl Display for LogLevel {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let s = match self {
-            LogLevel::Trace => "trace",
-            LogLevel::Debug => "debug",
-        };
-        write!(f, "{s}")
-    }
-}
-
-/// Config for an individual Clickhouse Replica
-pub struct ReplicaConfig {
-    pub logger: LogConfig,
-    pub macros: Macros,
-    pub listen_host: String,
-    pub http_port: u16,
-    pub tcp_port: u16,
-    pub interserver_http_port: u16,
-    pub remote_servers: RemoteServers,
-    pub keepers: KeeperConfigsForReplica,
-    pub data_path: Utf8PathBuf,
-}
-
-impl ReplicaConfig {
-    pub fn to_xml(&self) -> String {
-        let ReplicaConfig {
-            logger,
-            macros,
-            listen_host,
-            http_port,
-            tcp_port,
-            interserver_http_port,
-            remote_servers,
-            keepers,
-            data_path,
-        } = self;
-        let logger = logger.to_xml();
-        let cluster = macros.cluster.clone();
-        let id = macros.replica;
-        let macros = macros.to_xml();
-        let keepers = keepers.to_xml();
-        let remote_servers = remote_servers.to_xml();
-        let user_files_path = data_path.clone().join("user_files");
-        //let access_path = data_path.clone().join("access");
-        let format_schema_path = data_path.clone().join("format_schemas");
-        format!(
-            "
-<clickhouse>
-{logger}
-    <path>{data_path}</path>
-
-    <profiles>
-        <default>
-            <load_balancing>random</load_balancing>
-        </default>
-
-    </profiles>
-
-    <users>
-        <default>
-            <password></password>
-            <networks>
-                <ip>::/0</ip>
-            </networks>
-            <profile>default</profile>
-            <quota>default</quota>
-        </default>
-    </users>
-
-    <quotas>
-        <default>
-            <interval>
-                <duration>3600</duration>
-                <queries>0</queries>
-                <errors>0</errors>
-                <result_rows>0</result_rows>
-                <read_rows>0</read_rows>
-                <execution_time>0</execution_time>
-            </interval>
-        </default>
-    </quotas>
-
-    <user_files_path>{user_files_path}</user_files_path>
-    <default_profile>default</default_profile>
-    <default_profile>default</default_profile>
-    <format_schema_path>{format_schema_path}</format_schema_path>
-    <display_name>{cluster}-{id}</display_name>
-    <listen_host>{listen_host}</listen_host>
-    <http_port>{http_port}</http_port>
-    <tcp_port>{tcp_port}</tcp_port>
-    <interserver_http_port>{interserver_http_port}</interserver_http_port>
-    <interserver_http_host>::1</interserver_http_host>
-    <distributed_ddl>
-        <!-- Cleanup settings (active tasks will not be removed) -->
-
-        <!-- Controls task TTL (default 1 week) -->
-        <task_max_lifetime>604800</task_max_lifetime>
-
-        <!-- Controls how often cleanup should be performed (in seconds) -->
-        <cleanup_delay_period>60</cleanup_delay_period>
-
-        <!-- Controls how many tasks could be in the queue -->
-        <max_tasks_in_queue>1000</max_tasks_in_queue>
-     </distributed_ddl>
-{macros}
-{remote_servers}
-{keepers}
-
-</clickhouse>
-"
-        )
-    }
-}
-
-pub struct Macros {
-    pub shard: u64,
-    pub replica: u64,
-    pub cluster: String,
-}
-
-impl Macros {
-    pub fn to_xml(&self) -> String {
-        let Macros {
-            shard,
-            replica,
-            cluster,
-        } = self;
-        format!(
-            "
-    <macros>
-        <shard>{shard}</shard>
-        <replica>{replica}</replica>
-        <cluster>{cluster}</cluster>
-    </macros>"
-        )
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct RemoteServers {
-    pub cluster: String,
-    pub secret: String,
-    pub replicas: Vec<ServerConfig>,
-}
-
-impl RemoteServers {
-    pub fn to_xml(&self) -> String {
-        let RemoteServers {
-            cluster,
-            secret,
-            replicas,
-        } = self;
-
-        let mut s = format!(
-            "
-    <remote_servers replace=\"true\">
-        <{cluster}>
-            <secret>{secret}</secret>
-            <shard>
-                <internal_replication>true</internal_replication>"
-        );
-
-        for r in replicas {
-            let ServerConfig { host, port } = r;
-            s.push_str(&format!(
-                "
-                <replica>
-                    <host>{host}</host>
-                    <port>{port}</port>
-                </replica>"
-            ));
-        }
-
-        s.push_str(&format!(
-            "
-            </shard>
-        </{cluster}>
-    </remote_servers>
-        "
-        ));
-
-        s
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct KeeperConfigsForReplica {
-    pub nodes: Vec<ServerConfig>,
-}
-
-impl KeeperConfigsForReplica {
-    pub fn to_xml(&self) -> String {
-        let mut s = String::from("    <zookeeper>");
-        for node in &self.nodes {
-            let ServerConfig { host, port } = node;
-            s.push_str(&format!(
-                "
-        <node>
-            <host>{host}</host>
-            <port>{port}</port>
-        </node>",
-            ));
-        }
-        s.push_str("\n    </zookeeper>");
-        s
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct ServerConfig {
-    pub host: String,
-    pub port: u16,
-}
-
-pub struct LogConfig {
-    pub level: LogLevel,
-    pub log: Utf8PathBuf,
-    pub errorlog: Utf8PathBuf,
-    // TODO: stronger type?
-    pub size: String,
-    pub count: usize,
-}
-
-impl LogConfig {
-    pub fn to_xml(&self) -> String {
-        let LogConfig {
-            level,
-            log,
-            errorlog,
-            size,
-            count,
-        } = &self;
-        format!(
-            "
-    <logger>
-        <level>{level}</level>
-        <log>{log}</log>
-        <errorlog>{errorlog}</errorlog>
-        <size>{size}</size>
-        <count>{count}</count>
-    </logger>
-"
-        )
-    }
-}
-
-pub struct KeeperCoordinationSettings {
-    pub operation_timeout_ms: u32,
-    pub session_timeout_ms: u32,
-    pub raft_logs_level: LogLevel,
-}
-
-pub struct RaftServers {
-    pub servers: Vec<RaftServerConfig>,
-}
-
-impl RaftServers {
-    pub fn to_xml(&self) -> String {
-        let mut s = String::new();
-        for server in &self.servers {
-            let RaftServerConfig { id, hostname, port } = server;
-            s.push_str(&format!(
-                "
-            <server>
-                <id>{id}</id>
-                <hostname>{hostname}</hostname>
-                <port>{port}</port>
-            </server>
-            "
-            ));
-        }
-
-        s
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct RaftServerConfig {
-    pub id: u64,
-    pub hostname: String,
-    pub port: u16,
-}
-
-/// Config for an individual Clickhouse Keeper
-pub struct KeeperConfig {
-    pub logger: LogConfig,
-    pub listen_host: String,
-    pub tcp_port: u16,
-    pub server_id: u64,
-    pub log_storage_path: Utf8PathBuf,
-    pub snapshot_storage_path: Utf8PathBuf,
-    pub coordination_settings: KeeperCoordinationSettings,
-    pub raft_config: RaftServers,
-}
-
-impl KeeperConfig {
-    pub fn to_xml(&self) -> String {
-        let KeeperConfig {
-            logger,
-            listen_host,
-            tcp_port,
-            server_id,
-            log_storage_path,
-            snapshot_storage_path,
-            coordination_settings,
-            raft_config,
-        } = self;
-        let logger = logger.to_xml();
-        let KeeperCoordinationSettings {
-            operation_timeout_ms,
-            session_timeout_ms,
-            raft_logs_level,
-        } = coordination_settings;
-        let raft_servers = raft_config.to_xml();
-        format!(
-            "
-<clickhouse>
-{logger}
-    <listen_host>{listen_host}</listen_host>
-    <keeper_server>
-        <tcp_port>{tcp_port}</tcp_port>
-        <server_id>{server_id}</server_id>
-        <log_storage_path>{log_storage_path}</log_storage_path>
-        <snapshot_storage_path>{snapshot_storage_path}</snapshot_storage_path>
-        <coordination_settings>
-            <operation_timeout_ms>{operation_timeout_ms}</operation_timeout_ms>
-            <session_timeout_ms>{session_timeout_ms}</session_timeout_ms>
-            <raft_logs_level>{raft_logs_level}</raft_logs_level>
-        </coordination_settings>
-        <raft_configuration>
-{raft_servers}
-        </raft_configuration>
-    </keeper_server>
-
-</clickhouse>
-"
-        )
-    }
-}
+mod config;
+use config::*;
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -380,20 +40,46 @@ enum Commands {
         #[arg(short, long)]
         path: Utf8PathBuf,
     },
+
+    /// Show metadata about the deployment
+    Show {
+        /// Root path of all configuration
+        #[arg(short, long)]
+        path: Utf8PathBuf,
+    },
+
+    /// Add a keeper node to the keeper cluster
+    AddKeeper {
+        /// Root path of all configuration
+        #[arg(short, long)]
+        path: Utf8PathBuf,
+    },
+
+    /// Remove a keeper node
+    RemoveKeeperNode {
+        /// Root path of all configuration
+        #[arg(short, long)]
+        path: Utf8PathBuf,
+    },
 }
 
 fn main() {
     let cli = Cli::parse();
-    match cli.command {
+    let res = match cli.command {
         Commands::GenConfig {
             path,
             num_keepers,
             num_replicas,
-        } => {
-            generate_config(path, num_keepers, num_replicas);
-        }
+        } => generate_config(path, num_keepers, num_replicas),
         Commands::Deploy { path } => deploy(path),
+        Commands::Show { path } => show(path),
+        Commands::AddKeeper { path } => add_keeper(path),
+        Commands::RemoveKeeperNode { path } => Ok(()),
     };
+
+    if let Err(e) = res {
+        println!("Error: {e}");
+    }
 }
 
 const KEEPER_BASE_PORT: u16 = 20000;
@@ -405,10 +91,102 @@ const CLICKHOUSE_BASE_INTERSERVER_HTTP_PORT: u16 = 24000;
 /// We put things in a subdirectory of the user path for easy cleanup
 const DEPLOYMENT_DIR: &str = "deployment";
 
-/// Deploy our clickhouse replicas and keeper cluster
-fn deploy(path: Utf8PathBuf) {
+/// The name of the file where `ClickwardMetadata` lives. This is *always*
+/// directly below <path>/deployment.
+const CLICKWARD_META_FILENAME: &str = "clickward-metadata.json";
+
+/// Metadata stored for use by clickward
+///
+/// This prevents the need to parse XML and only includes what we need to
+/// implement commands.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ClickwardMetadata {
+    /// IDs of keepers that are currently part of the cluster
+    /// We never reuse IDs.
+    pub keeper_ids: BTreeSet<u64>,
+
+    /// The maximum allocated keeper_id so far
+    /// We only ever increment when adding a new id.
+    pub max_keeper_id: u64,
+}
+
+impl ClickwardMetadata {
+    pub fn new(keeper_ids: BTreeSet<u64>) -> ClickwardMetadata {
+        ClickwardMetadata {
+            keeper_ids,
+            max_keeper_id: 3,
+        }
+    }
+
+    pub fn add_keeper(&mut self) -> u64 {
+        self.max_keeper_id += 1;
+        self.keeper_ids.insert(self.max_keeper_id);
+        self.max_keeper_id
+    }
+
+    pub fn load(deployment_dir: &Utf8Path) -> Result<ClickwardMetadata> {
+        let path = deployment_dir.join(CLICKWARD_META_FILENAME);
+        let json =
+            std::fs::read_to_string(&path).with_context(|| format!("failed to read {path}"))?;
+        let meta = serde_json::from_str(&json)?;
+        Ok(meta)
+    }
+
+    pub fn save(&self, deployment_dir: &Utf8Path) -> Result<()> {
+        let path = deployment_dir.join(CLICKWARD_META_FILENAME);
+        let json = serde_json::to_string(self)?;
+        std::fs::write(&path, &json).with_context(|| format!("Failed to write {path}"))?;
+        Ok(())
+    }
+}
+
+fn show(path: Utf8PathBuf) -> Result<()> {
     let path = path.join(DEPLOYMENT_DIR);
-    let dirs: Vec<_> = path.read_dir_utf8().unwrap().collect();
+    let meta = ClickwardMetadata::load(&path)?;
+    println!("{:#?}", meta);
+    Ok(())
+}
+
+/// Add a node to clickhouse keeper config at all replicas and start the new
+/// keeper
+fn add_keeper(path: Utf8PathBuf) -> Result<()> {
+    let path = path.join(DEPLOYMENT_DIR);
+    let mut meta = ClickwardMetadata::load(&path)?;
+    let new_id = meta.add_keeper();
+
+    println!("Updating config to include new keeper: {new_id}");
+
+    // The writes from the following two functions aren't transactional
+    // Don't worry about it.
+    meta.save(&path)?;
+    generate_keeper_config(&path, meta.keeper_ids.clone())?;
+    start_keeper(&path, new_id);
+
+    Ok(())
+}
+
+fn start_keeper(path: &Utf8Path, id: u64) {
+    let dir = path.join(format!("keeper-{id}"));
+    println!("Deploying keeper: {dir}");
+    let config = dir.join("keeper-config.xml");
+    let pidfile = dir.join("keeper.pid");
+    Command::new("clickhouse")
+        .arg("keeper")
+        .arg("-C")
+        .arg(config)
+        .arg("--pidfile")
+        .arg(pidfile)
+        .stdin(Stdio::null())
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .expect("Failed to start keeper");
+}
+
+/// Deploy our clickhouse replicas and keeper cluster
+fn deploy(path: Utf8PathBuf) -> Result<()> {
+    let path = path.join(DEPLOYMENT_DIR);
+    let dirs: Vec<_> = path.read_dir_utf8()?.collect();
 
     // Find all keeper replicas them
     let keeper_dirs = dirs.iter().filter_map(|e| {
@@ -464,17 +242,25 @@ fn deploy(path: Utf8PathBuf) {
             .spawn()
             .expect("Failed to start clickhouse server");
     }
+    Ok(())
 }
 
 /// Generate configuration for our clusters
-fn generate_config(path: Utf8PathBuf, num_keepers: u64, num_replicas: u64) {
+fn generate_config(path: Utf8PathBuf, num_keepers: u64, num_replicas: u64) -> Result<()> {
     let path = path.join(DEPLOYMENT_DIR);
     std::fs::create_dir_all(&path).unwrap();
-    generate_clickhouse_config(path.clone(), num_keepers, num_replicas);
-    generate_keeper_config(path, num_keepers);
+    generate_clickhouse_config(&path, num_keepers, num_replicas)?;
+
+    let keeper_ids: BTreeSet<u64> = (1..=num_keepers).collect();
+    generate_keeper_config(&path, keeper_ids.clone())?;
+
+    let meta = ClickwardMetadata::new(keeper_ids);
+    meta.save(&path)?;
+
+    Ok(())
 }
 
-fn generate_clickhouse_config(path: Utf8PathBuf, num_keepers: u64, num_replicas: u64) {
+fn generate_clickhouse_config(path: &Utf8Path, num_keepers: u64, num_replicas: u64) -> Result<()> {
     let cluster = "test_cluster".to_string();
 
     let servers: Vec<_> = (1..=num_replicas)
@@ -501,7 +287,7 @@ fn generate_clickhouse_config(path: Utf8PathBuf, num_keepers: u64, num_replicas:
     for i in 1..=num_replicas {
         let dir: Utf8PathBuf = [path.as_str(), &format!("clickhouse-{i}")].iter().collect();
         let logs: Utf8PathBuf = dir.join("logs");
-        std::fs::create_dir_all(&logs).unwrap();
+        std::fs::create_dir_all(&logs)?;
         let log = logs.join("clickhouse.log");
         let errorlog = logs.join("clickhouse.err.log");
         let data_path = dir.join("data");
@@ -526,24 +312,26 @@ fn generate_clickhouse_config(path: Utf8PathBuf, num_keepers: u64, num_replicas:
             keepers: keepers.clone(),
             data_path,
         };
-        let mut f = File::create(dir.join("clickhouse-config.xml")).unwrap();
-        f.write_all(config.to_xml().as_bytes()).unwrap();
-        f.flush().unwrap();
+        let mut f = File::create(dir.join("clickhouse-config.xml"))?;
+        f.write_all(config.to_xml().as_bytes())?;
+        f.flush()?;
     }
+    Ok(())
 }
 
-fn generate_keeper_config(path: Utf8PathBuf, num_keepers: u64) {
-    let raft_servers: Vec<_> = (1..=num_keepers)
+fn generate_keeper_config(path: &Utf8Path, keeper_ids: BTreeSet<u64>) -> Result<()> {
+    let raft_servers: Vec<_> = keeper_ids
+        .iter()
         .map(|id| RaftServerConfig {
-            id,
+            id: *id,
             hostname: "::1".to_string(),
-            port: RAFT_BASE_PORT + id as u16,
+            port: RAFT_BASE_PORT + *id as u16,
         })
         .collect();
-    for i in 1..=num_keepers {
-        let dir: Utf8PathBuf = [path.as_str(), &format!("keeper-{i}")].iter().collect();
+    for id in keeper_ids.iter() {
+        let dir: Utf8PathBuf = [path.as_str(), &format!("keeper-{id}")].iter().collect();
         let logs: Utf8PathBuf = dir.join("logs");
-        std::fs::create_dir_all(&logs).unwrap();
+        std::fs::create_dir_all(&logs)?;
         let log = logs.join("clickhouse-keeper.log");
         let errorlog = logs.join("clickhouse-keeper.err.log");
         let config = KeeperConfig {
@@ -555,8 +343,8 @@ fn generate_keeper_config(path: Utf8PathBuf, num_keepers: u64) {
                 count: 1,
             },
             listen_host: "::1".to_string(),
-            tcp_port: KEEPER_BASE_PORT + i as u16,
-            server_id: i,
+            tcp_port: KEEPER_BASE_PORT + *id as u16,
+            server_id: *id,
             log_storage_path: dir.join("coordination").join("log"),
             snapshot_storage_path: dir.join("coordination").join("snapshots"),
             coordination_settings: KeeperCoordinationSettings {
@@ -568,8 +356,9 @@ fn generate_keeper_config(path: Utf8PathBuf, num_keepers: u64) {
                 servers: raft_servers.clone(),
             },
         };
-        let mut f = File::create(dir.join("keeper-config.xml")).unwrap();
-        f.write_all(config.to_xml().as_bytes()).unwrap();
-        f.flush().unwrap();
+        let mut f = File::create(dir.join("keeper-config.xml"))?;
+        f.write_all(config.to_xml().as_bytes())?;
+        f.flush()?;
     }
+    Ok(())
 }
